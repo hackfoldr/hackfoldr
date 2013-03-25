6117584cec8664a38e024a70a70dfb6f7ff1a85a
Audrey Tang <audreyt@audreyt.org>
2013-03-26 02:37:48 +0800

    * Build + Deploy UI tweaks from tkirby

--- js/app.js
+++ js/app.js
@@ -1,1 +1,289 @@
-(function(){"use strict";var e=typeof window!="undefined"?window:global;if(typeof e.require=="function")return;var t={},n={},r=function(e,t){return{}.hasOwnProperty.call(e,t)},i=function(e,t){var n=[],r,i;/^\.\.?(\/|$)/.test(t)?r=[e,t].join("/").split("/"):r=t.split("/");for(var s=0,o=r.length;s<o;s++)i=r[s],i===".."?n.pop():i!=="."&&i!==""&&n.push(i);return n.join("/")},s=function(e){return e.split("/").slice(0,-1).join("/")},o=function(t){return function(n){var r=s(t),o=i(r,n);return e.require(o)}},u=function(e,t){var r={id:e,exports:{}};t(r.exports,o(e),r);var i=n[e]=r.exports;return i},a=function(e){var s=i(e,".");if(r(n,s))return n[s];if(r(t,s))return u(s,t[s]);var o=i(s,"./index");if(r(n,o))return n[o];if(r(t,o))return u(o,t[o]);throw new Error('Cannot find module "'+e+'"')},f=function(e){for(var n in e)r(e,n)&&(t[n]=e[n])};e.require=a,e.require.define=f,e.require.brunch=!0})(),function(){var e;angular.module("scroll",[]).value("$anchorScroll",angular.noop),e=angular.module("app",["ui","ngCookies","ngResource","app.controllers","scroll"]),e.config(["$routeProvider","$locationProvider"].concat(function(e,t,n){return e.when("/:hackId",{templateUrl:"/partials/app/hack.html"}).when("/:hackId/:docId",{templateUrl:"/partials/app/hack.html"}).when("/about",{templateUrl:"/partials/app/about.html"}).otherwise({redirectTo:"/"}),t.html5Mode(!1)})),window.App=e}.call(this),function(){function t(e,t){var n={}.hasOwnProperty;for(var r in t)n.call(t,r)&&(e[r]=t[r]);return e}var e=[].slice;angular.module("app.controllers",[]).controller({AppCtrl:["$scope","$location","$resource","$rootScope"].concat(function(e,t,n,r){return e.$location=t,e.$watch("$location.path()",function(t){return t||(t="/"),e.activeNavId=t,e}),e.getClass=function(t){return e.activeNavId.substring(0,t.length===t)?"active":""}})}).controller({HackFolderCtrl:["$scope","$routeParams","HackFolder"].concat(function(e,n,r){var i;console.log(n),t(e,{hasViewMode:function(e){return e.match(/g(doc|present|draw)/)},sortableOptions:{update:function(){return console.log("notyetupdated")}},iframes:r.iframes,docs:r.docs,activate:r.activate}),e.$watch("hackId",function(t){return r.getIndex(t,!1,function(){return e.$watch("docId",function(e){return r.activate(e)})})}),e.hackId=(i=n.hackId)?i:"s8r4l008sk",console.log(e.hackId);if(n.docId)return e.docId=n.docId})}).directive("resize",["$window"].concat(function(e){return function(t){return t.width=e.innerWidth,t.height=e.innerHeight,angular.element(e).bind("resize",function(){return t.$apply(function(){return t.width=e.innerWidth,t.height=e.innerHeight})})}})).factory({HackFolder:["$http"].concat(function(t){var n,r,i;return n={},r=[],{iframes:n,docs:r,activate:function(e,t){var i,s,o,u,a,f;return t==null&&(t=!1),s=function(){var t,n,s,o=[];for(t=0,s=(n=r).length;t<s;++t)i=n[t],i.id===e&&o.push(i);return o}()[0],o=s.type,u=t?"edit":"view",a=function(){var t;switch(t=[o],!1){case"gdoc"!==t[0]:return"https://docs.google.com/document/d/"+e+"/"+u;case"gsheet"!==t[0]:return"https://docs.google.com/spreadsheet/ccc?key="+e;case"gpresent"!==t[0]:return"https://docs.google.com/presentation/d/"+e+"/"+u;case"gdraw"!==t[0]:return"https://docs.google.com/drawings/d/"+e+"/"+u;case"gsheet"!==t[0]:return"https://docs.google.com/spreadsheet/ccc?key="+e;case"hackpad"!==t[0]:return"https://hackpad.com/"+e;case"ethercalc"!==t[0]:return"http://ethercalc.com/"+e}}(),console.log("activate",e,n[e]),(f=n[e])?(f.src=a,f.mode=u,f):n[e]={src:a,doc:s,mode:u}},getIndex:function(n,s,o){return i===n&&!s?o(r):t.get("http://www.ethercalc.com/_/"+n+"/csv").success(function(t){var s,u,a,f,l,c,h,p,d,v,m;i=n,r.splice(0,-1),u=[];for(a=0,l=(f=t.split(/\n/)).length;a<l;++a){c=f[a];if(c){h=c.split(/,/),p=h[0],d=h[1],v=e.call(h,2);switch(h=[p],!1){case!(m=/^https?:\/\/www\.ethercalc\.com\/(.*)/.exec(h[0])):u.push({type:"ethercalc",id:m[1],title:d});break;case!(m=/https:\/\/docs\.google\.com\/document\/(?:d\/)?([^\/]+)\//.exec(h[0])):u.push({type:"gdoc",id:m[1],title:d});break;case!(m=/https:\/\/docs\.google\.com\/spreadsheet\/ccc\?key=([^\/?&]+)/.exec(h[0])):u.push({type:"gsheet",id:m[1],title:d});break;case!(m=/https:\/\/docs\.google\.com\/drawings\/(?:d\/)?([^\/]+)\//.exec(h[0])):u.push({type:"gdraw",id:m[1],title:d});break;case!(m=/https:\/\/docs\.google\.com\/presentation\/(?:d\/)?([^\/]+)\//.exec(h[0])):u.push({type:"gpresent",id:m[1],title:d});break;case!(m=/https?:\/\/hackpad\.com\/(?:.*?)-([\w]+)(\#.*)?$/.exec(h[0])):u.push({type:"hackpad",id:m[1],title:d});break;default:u.push(console.log("unrecognized",p))}}}return s=u,r.splice.apply(r,[0,-1].concat(e.call(s.filter(function(e){return e!=null})))),o(r)})}}})})}.call(this),function(){angular.element(document).ready(function(){return angular.bootstrap(document,["app"])})}.call(this)
+(function(/*! Brunch !*/) {
+  'use strict';
+
+  var globals = typeof window !== 'undefined' ? window : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+
+  var has = function(object, name) {
+    return ({}).hasOwnProperty.call(object, name);
+  };
+
+  var expand = function(root, name) {
+    var results = [], parts, part;
+    if (/^\.\.?(\/|$)/.test(name)) {
+      parts = [root, name].join('/').split('/');
+    } else {
+      parts = name.split('/');
+    }
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var dir = dirname(path);
+      var absolute = expand(dir, name);
+      return globals.require(absolute);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    definition(module.exports, localRequire(name), module);
+    var exports = cache[name] = module.exports;
+    return exports;
+  };
+
+  var require = function(name) {
+    var path = expand(name, '.');
+
+    if (has(cache, path)) return cache[path];
+    if (has(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has(cache, dirIndex)) return cache[dirIndex];
+    if (has(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '"');
+  };
+
+  var define = function(bundle) {
+    for (var key in bundle) {
+      if (has(bundle, key)) {
+        modules[key] = bundle[key];
+      }
+    }
+  }
+
+  globals.require = require;
+  globals.require.define = define;
+  globals.require.brunch = true;
+})();
+
+(function() {
+  var App;
+angular.module('scroll', []).value('$anchorScroll', angular.noop);
+App = angular.module('app', ['ui', 'ngCookies', 'ngResource', 'app.controllers', 'scroll']);
+App.config(['$routeProvider', '$locationProvider'].concat(function($routeProvider, $locationProvider, config){
+  $routeProvider.when('/:hackId', {
+    templateUrl: '/partials/app/hack.html'
+  }).when('/:hackId/:docId', {
+    templateUrl: '/partials/app/hack.html'
+  }).when('/about', {
+    templateUrl: '/partials/app/about.html'
+  }).otherwise({
+    redirectTo: '/'
+  });
+  return $locationProvider.html5Mode(false);
+}));
+window.App = App;
+}).call(this);
+
+(function() {
+  var slice$ = [].slice;
+angular.module('app.controllers', []).controller({
+  AppCtrl: ['$scope', '$location', '$resource', '$rootScope'].concat(function(s, $location, $resource, $rootScope){
+    s.$location = $location;
+    s.$watch('$location.path()', function(activeNavId){
+      activeNavId || (activeNavId = '/');
+      return s.activeNavId = activeNavId, s;
+    });
+    return s.getClass = function(id){
+      if (s.activeNavId.substring(0, id.length === id)) {
+        return 'active';
+      } else {
+        return '';
+      }
+    };
+  })
+}).controller({
+  HackFolderCtrl: ['$scope', '$routeParams', 'HackFolder'].concat(function($scope, $routeParams, HackFolder){
+    var that;
+    console.log($routeParams);
+    import$($scope, {
+      hasViewMode: function(it){
+        return it.match(/g(doc|present|draw)/);
+      },
+      sortableOptions: {
+        update: function(){
+          return console.log('notyetupdated');
+        }
+      },
+      iframes: HackFolder.iframes,
+      docs: HackFolder.docs,
+      activate: HackFolder.activate
+    });
+    $scope.$watch('hackId', function(hackId){
+      return HackFolder.getIndex(hackId, false, function(){
+        return $scope.$watch('docId', function(docId){
+          return HackFolder.activate(docId);
+        });
+      });
+    });
+    $scope.hackId = (that = $routeParams.hackId) ? that : 's8r4l008sk';
+    console.log($scope.hackId);
+    if ($routeParams.docId) {
+      return $scope.docId = $routeParams.docId;
+    }
+  })
+}).directive('resize', ['$window'].concat(function($window){
+  return function(scope){
+    scope.width = $window.innerWidth;
+    scope.height = $window.innerHeight;
+    return angular.element($window).bind('resize', function(){
+      return scope.$apply(function(){
+        scope.width = $window.innerWidth;
+        return scope.height = $window.innerHeight;
+      });
+    });
+  };
+})).factory({
+  HackFolder: ['$http'].concat(function($http){
+    var iframes, docs, hackId;
+    iframes = {};
+    docs = [];
+    return {
+      iframes: iframes,
+      docs: docs,
+      activate: function(id, edit){
+        var d, doc, type, mode, src, that;
+        edit == null && (edit = false);
+        doc = (function(){
+          var i$, ref$, len$, results$ = [];
+          for (i$ = 0, len$ = (ref$ = docs).length; i$ < len$; ++i$) {
+            d = ref$[i$];
+            if (d.id === id) {
+              results$.push(d);
+            }
+          }
+          return results$;
+        }())[0], type = doc.type;
+        mode = edit ? 'edit' : 'view';
+        src = (function(){
+          var ref$;
+          switch (ref$ = [type], false) {
+          case 'gdoc' !== ref$[0]:
+            return "https://docs.google.com/document/d/" + id + "/" + mode;
+          case 'gsheet' !== ref$[0]:
+            return "https://docs.google.com/spreadsheet/ccc?key=" + id;
+          case 'gpresent' !== ref$[0]:
+            return "https://docs.google.com/presentation/d/" + id + "/" + mode;
+          case 'gdraw' !== ref$[0]:
+            return "https://docs.google.com/drawings/d/" + id + "/" + mode;
+          case 'gsheet' !== ref$[0]:
+            return "https://docs.google.com/spreadsheet/ccc?key=" + id;
+          case 'hackpad' !== ref$[0]:
+            return "https://hackpad.com/" + id;
+          case 'ethercalc' !== ref$[0]:
+            return "http://ethercalc.com/" + id;
+          }
+        }());
+        console.log('activate', id, iframes[id]);
+        if (that = iframes[id]) {
+          return that.src = src, that.mode = mode, that;
+        } else {
+          return iframes[id] = {
+            src: src,
+            doc: doc,
+            mode: mode
+          };
+        }
+      },
+      getIndex: function(id, force, cb){
+        if (hackId === id && !force) {
+          return cb(docs);
+        }
+        return $http.get("http://www.ethercalc.com/_/" + id + "/csv").success(function(csv){
+          var entries, res$, i$, ref$, len$, line, ref1$, url, title, rest, that;
+          hackId = id;
+          docs.splice(0, -1);
+          res$ = [];
+          for (i$ = 0, len$ = (ref$ = csv.split(/\n/)).length; i$ < len$; ++i$) {
+            line = ref$[i$];
+            if (line) {
+              ref1$ = line.split(/,/), url = ref1$[0], title = ref1$[1], rest = slice$.call(ref1$, 2);
+              switch (ref1$ = [url], false) {
+              case !(that = /^https?:\/\/www\.ethercalc\.com\/(.*)/.exec(ref1$[0])):
+                res$.push({
+                  type: 'ethercalc',
+                  id: that[1],
+                  title: title
+                });
+                break;
+              case !(that = /https:\/\/docs\.google\.com\/document\/(?:d\/)?([^\/]+)\//.exec(ref1$[0])):
+                res$.push({
+                  type: 'gdoc',
+                  id: that[1],
+                  title: title
+                });
+                break;
+              case !(that = /https:\/\/docs\.google\.com\/spreadsheet\/ccc\?key=([^\/?&]+)/.exec(ref1$[0])):
+                res$.push({
+                  type: 'gsheet',
+                  id: that[1],
+                  title: title
+                });
+                break;
+              case !(that = /https:\/\/docs\.google\.com\/drawings\/(?:d\/)?([^\/]+)\//.exec(ref1$[0])):
+                res$.push({
+                  type: 'gdraw',
+                  id: that[1],
+                  title: title
+                });
+                break;
+              case !(that = /https:\/\/docs\.google\.com\/presentation\/(?:d\/)?([^\/]+)\//.exec(ref1$[0])):
+                res$.push({
+                  type: 'gpresent',
+                  id: that[1],
+                  title: title
+                });
+                break;
+              case !(that = /https?:\/\/hackpad\.com\/(?:.*?)-([\w]+)(\#.*)?$/.exec(ref1$[0])):
+                res$.push({
+                  type: 'hackpad',
+                  id: that[1],
+                  title: title
+                });
+                break;
+              default:
+                res$.push(console.log('unrecognized', url));
+              }
+            }
+          }
+          entries = res$;
+          docs.splice.apply(docs, [0, -1].concat(slice$.call(entries.filter(function(it){
+            return it != null;
+          }))));
+          return cb(docs);
+        });
+      }
+    };
+  })
+});
+function import$(obj, src){
+  var own = {}.hasOwnProperty;
+  for (var key in src) if (own.call(src, key)) obj[key] = src[key];
+  return obj;
+}
+}).call(this);
+
+(function() {
+  angular.element(document).ready(function(){
+  return angular.bootstrap(document, ['app']);
+});
+}).call(this);
+
