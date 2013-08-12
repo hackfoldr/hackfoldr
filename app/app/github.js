b6b39a482d75a4474a0514b733c27140f0789f9d
Jeff Hung <jeff.cc.hung@gmail.com>
2013-08-12 16:53:04 +0800

    Show issue lists.

--- app/app/github.js
+++ app/app/github.js
@@ -1,4 +1,8 @@
 var Github = (function($) {
+	var copy_fields = function(to, from, fields) {
+		$.each(fields, function(j, key) { to[key] = from[key]; });
+	};
+
 	var    re_ghurl = /^((http|https):\/\/github\.com\/([^\/]+)\/([^\/]+))(\/.*)?$/;
 	var parse_ghurl = function(url) {
 		if (url) {
@@ -21,26 +25,36 @@ var Github = (function($) {
 			return r ? r.repo : null;
 		},
 		load_issues: function(url, callback) {
-			var r = parse_ghurl(url);
-			var api = 'https://api.github.com/repos/' + r.name + '/' + r.repo + '/issues';
-			$.getJSON(api, function(data) {
-				// Only select these fields: title, state, body, html_url, label.name
-				var issues = [];
-				$.each(data, function(i, datum) {
-					var issue = { label: [] };
-					$.each(
-						['title', 'state', 'body', 'html_url'],
-						function(j, key) {
-							issue[key] = datum[key];
-						}
-					);
-					$.each(datum.labels, function(k, label) {
-						issue.label.push(label.name);
-					});
-					issues.push(issue);
+			if ($.isArray(url)) {
+				console.log('url is array.');
+				console.log(url);
+				$.each(url, function(i, u) {
+					Github.load_issues(u, callback);
 				});
-				callback(issues);
-			});
+			} else {
+				console.log('loading ' + url);
+				var r = parse_ghurl(url);
+				var api = 'https://api.github.com/repos/' + r.name + '/' + r.repo + '/issues';
+				$.getJSON(api, function(data) {
+					// Only select these fields: title, state, body, html_url, label.name
+					var issues = [];
+					$.each(data, function(i, datum) {
+						console.log(datum);
+						var issue = {
+							repo: r.repo,
+							assignee: datum.assignee,
+							labels: []
+						};
+						copy_fields(issue, datum, ['title', 'state', 'body', 'html_url']);
+						$.each(datum.labels, function(k, label) {
+							issue.labels.push(label.name);
+						});
+						issue.label_str = issue.labels.join(':');
+						issues.push(issue);
+					});
+					callback(issues);
+				});
+			}
 		},
 	};
 })(jQuery);
@@ -51,5 +65,11 @@ angular.module("github", [])
 	return function(input) {
 		return Github.url_to_repo_name(input);
 	};
-});
+})
+.controller('IssueCtrl', [ '$scope', function($scope) {
+	Github.load_issues(
+		[ 'http://github.com/g0v/twbudget', 'http://github.com/g0v/hack.g0v.tw' ],
+		function(issues) { $scope.issues = issues.concat($scope.issues); }
+	);
+}]);
 
