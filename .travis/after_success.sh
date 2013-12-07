620c6e3c1c8508e84d2b0da08771f07aa9181da8
Chia-liang Kao <clkao@clkao.org>
2013-12-07 07:26:08 -0800

    base64 key

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -19,7 +19,7 @@ EOF
 ## add .travis/deploy-key.pub as your github project's deploy key
 ## make sure you git add only .travis/deploy-key.enc
 # password=`cat /dev/urandom | head -c 10000 | openssl sha1`
-# openssl aes-256-cbc -k "$password" -in .travis/deploy-key -out .travis/deploy-key.enc
+# openssl aes-256-cbc -k "$password" -in .travis/deploy-key -a -out .travis/deploy-key.enc
 # travis encrypt -r OWNER/REPONAME secret=$password
 ## add the result line to .travis.yml
 ## see also http://about.travis-ci.org/docs/user/encryption-keys/
