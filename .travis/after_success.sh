42d8a8307923ccc3b3b4b1e9a12cd5029df6d91f
Chia-liang Kao <clkao@clkao.org>
2013-12-07 06:48:06 -0800

    document procedure for creating encrypted deploy keys

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -14,6 +14,15 @@ CheckHostIP            no
 BatchMode              yes
 EOF
 
+# to create a deploy key and encrypt it for travis:
+# ssh-keygen -f .travis/deploy-key
+## add .travis/deploy-key.pub as your github project's deploy key
+## make sure you git add only .travis/deploy-key.enc
+# password=`cat /dev/urandom | head -c 10000 | openssl sha1`
+# openssl aes-256-cbc -k "$password" -in .travis/deploy-key -out .travis/deploy-key.enc
+# travis encrypt secret=$password
+## add the result line to .travis.yml
+## see also http://about.travis-ci.org/docs/user/encryption-keys/
 openssl aes-256-cbc -k "$secret" -in .travis/deploy-key.enc -d -a -out ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa # this key should have push access
 echo -e ">>> Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
